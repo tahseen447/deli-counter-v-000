3a9fc3d6480b3d59521adefa33538b0d38329610
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-22 21:22:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,6 @@ if(deli.size == 0)
   puts "The line is currently empty."
 else
   puts "The line is currently: "
-  puts deli.each_with_index {|name, index| "#{index+1}. #{name} "}
+  deli.each_with_index {|name, index| puts "#{index+1}. #{name} "}
 end
 end
